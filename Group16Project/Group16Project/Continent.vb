059934bdd45ffd2747f6ad7660383ca7194bb360
Jason Donnaghie <donnaghie13@gmail.com>
2022-09-13 13:36:39 +0200

    t

--- Group16Project/Group16Project/Continent.vb
+++ Group16Project/Group16Project/Continent.vb
@@ -1,6 +1,14 @@
 ﻿Option Strict On
 Option Explicit On
 Option Infer Off
+' *****************************************************************
+' Team Number: assigned to team
+' Team Member 1 Details: Surname, Initials (Student #)
+' Team Member 2 Details: Surname, Initials (Student #)
+' Team Member 3 Details: Surname, Initials (Student #)
+' Team Member 4 Details: e.g. Smith, J (202000001)
+' Practical: Team Project
+' *****************************************************************
 Public Class Continent
 
 End Class
--- Group16Project/Group16Project/Form1.vb
+++ Group16Project/Group16Project/Form1.vb
@@ -1,6 +1,14 @@
 ﻿Option Strict On
 Option Explicit On
 Option Infer Off
+' *****************************************************************
+' Team Number: assigned to team
+' Team Member 1 Details: Kuwandira, A (222062566)
+' Team Member 2 Details: Mkhabela, PL (222072645)
+' Team Member 3 Details: Phalane CK (222008253)
+' Team Member 4 Details: Donnaghie, JB (222098397)
+' Practical: Team Project
+' *****************************************************************
 Public Class frmGDP
 
 End Class
